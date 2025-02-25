5d0ceaafeb2895aa257bee81b2a6c421ca83d354
Fadelmystic <Mohamedfadelbeniche@gmail.com>
2025-02-25 20:13:03 +0000

    Delete Gestion résidence étudiant/Gestion-Residence-BENICHE directory

--- Gestion résidence étudiant/Gestion-Residence-BENICHE/src/main/java/com/example/prj/entity/PayementStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.prj.entity;
-
-public enum PayementStatus {
-    PENDING, // Le paiement est en attente
-    PAID,    // Le paiement a été effectué
-    CANCELLED, // Le paiement a été annulé
-    FAILED,  // Le paiement a échoué
-    REFUNDED // Le paiement a été remboursé
-}
--- Gestion résidence étudiant/Gestion-Residence-BENICHE/src/main/java/com/example/prj/entity/RoomStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.prj.entity;
-public enum RoomStatus {
-    AVAILABLE, // Room is available for allocation
-    OCCUPIED,  // Room is currently occupied by a student
-    MAINTENANCE // Room is under maintenance
-}
